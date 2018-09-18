a5278ce13f17f87b7984b4e83791a9b6a087be06
DaTunc <johannestunc@hotmail.com>
2018-09-18 09:48:36 +0200

    HelloWorld

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: !ahif
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:			Johannes Tunc
+ * ----------------------------------------------------------
+ * Description:
+ * The casual hello World
+ * ----------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Hello World\n");
+  return 0;
+}
